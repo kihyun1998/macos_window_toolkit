4355b3dd38ccf50c90f2a68daa72d4290209cc18
Ki Hyun Park <kihyun1998@penta.co.kr>
2025-08-22 09:56:42 +0900

    add exmaple

---
+++ example/android/app/src/main/kotlin/com/example/macos_window_toolkit_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.macos_window_toolkit_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import macos_window_toolkit
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MacosWindowToolkitPlugin.register(with: registry.registrar(forPlugin: "MacosWindowToolkitPlugin"))
+}
